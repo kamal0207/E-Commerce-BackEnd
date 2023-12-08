fae77ce6ed4ccec6171041ac0d527fd5384d2b43
Imoj OG <imojog@Imojs-Air.lan>
2023-12-08 05:23:39 -0600

    yo

--- medusa-config.js
+++ medusa-config.js
@@ -23,7 +23,7 @@ try {
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
-  process.env.ADMIN_CORS || "https://e-commerce-back-end-rust.vercel.app/login";
+  process.env.ADMIN_CORS || "https://e-commerce-back-end-rust.vercel.app/";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
